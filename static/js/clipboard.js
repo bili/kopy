827703179faa2fd07fddc17323d58da7f0a86356
bili <panning20@gmail.com>
2015-10-12 10:47:01 +0800

    init

---
+++ static/js/clipboard.js
@@ -0,0 +1,48 @@
+(function() {
+    var Clipboard, root;
+
+    Clipboard = {
+        _createTextArea: function() {
+            var textArea = document.createElement("textarea");
+            textArea.style.position = "absolute";
+            textArea.style.left = "-100%";
+            return textArea;
+        },
+        copy: function(data) {
+            if (window.clipboardData && window.clipboardData.setData) {
+                window.clipboardData.clearData("text");
+                window.clipboardData.setData("text", data);
+            } else {
+                var textArea = this._createTextArea();
+                textArea.value = data;
+                document.body.appendChild(textArea);
+                textArea.focus();
+                textArea.select();
+                document.execCommand("copy");
+                document.body.removeChild(textArea);
+            }
+        },
+        paste: function() {
+            var value = '';
+            if (window.clipboardData && window.clipboardData.getData) {
+                value = window.clipboardData.getData("text");
+            } else {
+                var textArea = this._createTextArea();
+                document.body.appendChild(textArea);
+                textArea.focus();
+                textArea.select();
+                if (document.execCommand('paste')) {
+                    document.execCommand("paste");
+                    value = textArea.value;
+                }
+                document.body.removeChild(textArea);
+            }
+            return value;
+        }
+    };
+
+    root = typeof exports !== "undefined" && exports !== null ? exports : window;
+
+    root.Clipboard = Clipboard;
+
+}).call(this);
---
+++ static/js/index.js
@@ -0,0 +1,7 @@
+$(function() {
+    $('.copy').on('click', function() {
+        var txt = $('.text').val();
+        Clipboard.copy(txt);
+        // Clipboard.paste();
+    });
+});
